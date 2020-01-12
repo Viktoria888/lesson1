40f1c682fdaee0da6a699b50b837a503d4be3807
nata-vika@list.ru <nata-vika@list.ru>
2020-01-12 14:14:17 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число в метрах: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Число полных километров: {num / 1000}");
+            Console.ReadKey();
+        }
+    }
+}
